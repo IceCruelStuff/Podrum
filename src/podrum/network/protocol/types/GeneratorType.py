41b88e9dd142d89a14f01a224b5cb8244f7d7c79
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-13 21:02:45 -0700

    Create GeneratorType.py

---
+++ src/podrum/network/protocol/types/GeneratorType.py
@@ -0,0 +1,18 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+class GeneratorType:
+    FINITE_OVERWORLD = 0
+    OVERWORLD = 1
+    FLAT = 2
+    NETHER = 3
+    THE_END = 4
