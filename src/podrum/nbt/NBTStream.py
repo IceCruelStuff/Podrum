4219f9305612ab11fb5db715d4628a800fed8e2c
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-15 16:04:32 -0700

    Create NBTStream.py

---
+++ src/podrum/nbt/NBTStream.py
@@ -0,0 +1,19 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from abc import ABCMeta, abstractmethod
+
+class NBTStream:
+    __metaclass__ = ABCMeta
+
+    buffer = ""
+    offset = 0
