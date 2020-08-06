8a8ba0a6a77fee1bc71f25dc1bdf67f56820b426
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-08-05 22:06:00 -0700

    Create R12ToCurrentBlockMapEntry.py

---
+++ src/podrum/network/convert/R12ToCurrentBlockMapEntry.py
@@ -0,0 +1,26 @@
+"""
+*  ____           _
+* |  _ \ ___   __| |_ __ _   _ _ __ ___
+* | |_) / _ \ / _` | '__| | | | '_ ` _ \
+* |  __/ (_) | (_| | |  | |_| | | | | | |
+* |_|   \___/ \__,_|_|   \__,_|_| |_| |_|
+*
+* Licensed under the Apache License, Version 2.0 (the "License")
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+from podrum.nbt.tag.CompoundTag import CompoundTag
+
+class R12ToCurrentBlockMapEntry:
+    id = None
+    meta = None
+    blockState = None
+
+    def __init__(self, id, meta, blockState):
+        self.id = id
+        self.meta = meta
+        self.blockState = blockState
+
+    def getId(self):
+        return self.id
