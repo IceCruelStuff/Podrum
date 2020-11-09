d3465e74491a9254faff915b32bce1c4f6177a4b
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 18:16:31 -0800

    Update ClientboundPacket.py

--- src/podrum/network/mcje/protocol/ClientboundPacket.py
+++ src/podrum/network/mcje/protocol/ClientboundPacket.py
@@ -10,7 +10,7 @@
 * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 """
 
-from podrum.network.mcje.protocol.Packet import Packet
+from podrum.network.NetBinaryStream import NetBinaryStream
 
-class ClientboundPacket(Packet):
+class ClientboundPacket(NetBinaryStream):
     pass
